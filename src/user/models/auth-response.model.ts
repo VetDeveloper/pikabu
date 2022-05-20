fc373ec6b9b4c03f89dfac89f1e366e8e829cc6b
Саша <sasa@purrweb.local>
2022-05-20 13:16:08 +0600

    fix(auth-response.model): fix user type UserModel -> ResponseUserModel

--- src/user/models/auth-response.model.ts
+++ src/user/models/auth-response.model.ts
@@ -1,10 +1,11 @@
 import { Field, ObjectType } from "@nestjs/graphql";
+import { ResponseUser } from "./response-user.model";
 import { UserModel } from "./user.model";
 
 @ObjectType()
 export class AuthResponse {
     @Field()
-    user : UserModel
+    user : ResponseUser
     @Field()
     accessToken: string
 }
--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -4,20 +4,17 @@ import { IsEmail, IsInt, IsPositive, IsString, Length } from 'class-validator';
 @ObjectType()
 export class UserModel {
   @Field(() => ID)
-  @IsInt()
-  @IsPositive()
   id: number;
 
-  @Field({ description: '' })
-  @IsEmail()
+  @Field()
   email: string;
 
   @Field()
-  @IsString()
-  @Length(6)
   password: string;
 
+  @Field()
   createdAt: Date;
 
+  @Field()
   updatedAt: Date;
 }
