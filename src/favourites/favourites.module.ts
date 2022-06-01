8a740d4f53254222c4734181e1af04b404f55c0f
Саша <sasa@purrweb.local>
2022-06-01 15:24:22 +0600

    feature(favourite.service): create new

--- src/favourites/favourites.module.ts
+++ src/favourites/favourites.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommentaryRepository } from 'src/commentary/commentary.repository';
+import { PostRepository } from 'src/post/post.repository';
 import { FavouritesRepository } from './favourites.repository';
 import { FavouriteMutationResolver } from './resolvers/mutation/mutation-favourite.resolver';
 import { FavouriteService } from './services/favourite.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FavouritesRepository])],
-  exports: [],
+  imports: [TypeOrmModule.forFeature([FavouritesRepository, PostRepository, CommentaryRepository])],
+  exports: [FavouriteService],
   providers: [FavouriteService, FavouriteMutationResolver],
 })
 export class FavouritesModule {}
--- src/favourites/services/favourite.service.ts
+++ src/favourites/services/favourite.service.ts
@@ -1,19 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CommentaryRepository } from 'src/commentary/commentary.repository';
+import { CommentaryEntity } from 'src/commentary/entities/commentary.entity';
+import { PostEntity } from 'src/post/entities/post.entity';
+import { PostRepository } from 'src/post/post.repository';
 import { FavouritesEntity } from '../entities/favourites.entity';
+import { EntityType } from '../enums/entity-type.enum';
 import { FavouritesRepository } from '../favourites.repository';
 import { CreateFavouriteInput } from '../inputs/create-favourite.input';
 
 @Injectable()
 export class FavouriteService {
-  constructor(private favouritesRepository: FavouritesRepository) {}
+  constructor(
+    private favouritesRepository: FavouritesRepository,
+    private postRepository: PostRepository,
+    private commentaryRepository: CommentaryRepository
+    ) {}
 
-  createFavourite(
+  async createFavourite(
     userId: number,
     dto: CreateFavouriteInput,
   ): Promise<FavouritesEntity> {
 
     
-    ////////////////// add logic for check exeptions
+    switch(dto.entityType) {
+      case EntityType.POST:
+        const post : PostEntity = await this.postRepository.findOne(dto.entityId);
+        if(!post) {
+          throw new NotFoundException();
+        }
+        break;
+      case EntityType.COMMENTARY:
+        const commentary: CommentaryEntity = await this.commentaryRepository.findOne(dto.entityId)
+        if(!commentary) {
+          throw new NotFoundException();
+        }
+        break;
+    }
 
     return this.favouritesRepository.save({
       ...dto,
